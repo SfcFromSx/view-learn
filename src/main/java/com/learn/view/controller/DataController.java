c2540044c4bd9319c136fa3b7082336c1ff57880
KEEP4M <zhaojiaxiang_xy312@163.com>
2022-01-27 19:14:01 +0800

    常用组件封装

--- src/main/java/com/learn/view/controller/DataController.java
+++ src/main/java/com/learn/view/controller/DataController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -41,10 +42,39 @@ public class DataController {
 
     @ResponseBody
     @RequestMapping("/learn/view/extract/gauge")
-    public List<ChartVo> extractGauge() {
+    public List<List<ChartVo>> extractGauge() {
+        List<List<ChartVo>> values = new ArrayList<>();
         List<ChartVo> value = new ArrayList<>();
         value.add(new ChartVo("",String.format("%.2f", new Random().nextDouble()*100.0), DEFAULT));
-        return value;
+        values.add(value);
+        return values;
+    }
+
+    @ResponseBody
+    @RequestMapping("/learn/view/extract/liquidFill")
+    public List<List<ChartVo>> extractLiquidFill() {
+        List<List<ChartVo>> values = new ArrayList<>();
+        List<ChartVo> value = new ArrayList<>();
+        value.add(new ChartVo("",String.format("%.2f", new Random().nextDouble()), DEFAULT));
+        value.add(new ChartVo("",String.format("%.2f", new Random().nextDouble()), DEFAULT));
+        value.add(new ChartVo("",String.format("%.2f", new Random().nextDouble()), DEFAULT));
+        values.add(value);
+        return values;
+    }
+
+    @ResponseBody
+    @RequestMapping("/learn/view/extract/map")
+    public List<List<ChartVo>> extractMap() {
+        List<List<ChartVo>> values = new ArrayList<>();
+        List<ChartVo> value = new ArrayList<>();
+        List<String> list = Arrays.asList("北京","天津","上海","重庆","河北","河南","云南","辽宁","黑龙江","湖南","安徽","山东",
+                "新疆","江苏","浙江","江西","湖北","广西","甘肃","山西","内蒙古","陕西","吉林","福建","贵州","广东","青海","西藏",
+                "四川","宁夏","海南","台湾","香港","澳门","南海诸岛");
+        for(int i=0;i<list.size();i++){
+            value.add(new ChartVo(list.get(i),String.valueOf(new Random().nextInt(2000)), DEFAULT));
+        }
+        values.add(value);
+        return values;
     }
 
     private List<ChartVo> getRandomData(String name) {
