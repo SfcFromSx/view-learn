39601332b8ecb9c2a1dca7fa134ae2441e3c2eeb
sfc <10161324+sfcsfc@user.noreply.gitee.com>
2022-02-11 15:25:17 +0800

    update main1

--- src/main/java/com/learn/view/controller/PreViewController.java
+++ src/main/java/com/learn/view/controller/PreViewController.java
@@ -10,6 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PreViewController {
 
+    @RequestMapping("/learn/view/preview/main1/{templateValue}")
+    public String main1(Model model, @PathVariable("templateValue") String templateValue) throws Exception {
+        ViewPara viewPara = H2DataSource.getTemplateParaByValue(templateValue);
+        model.addAttribute("para", viewPara);
+        return "view_main1";
+    }
+
     @RequestMapping("/learn/view/preview/main2/{templateValue}")
     public String main2(Model model, @PathVariable("templateValue") String templateValue) throws Exception {
         ViewPara viewPara = H2DataSource.getTemplateParaByValue(templateValue);
