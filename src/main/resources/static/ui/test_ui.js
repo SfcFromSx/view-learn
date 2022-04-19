cbbae92640a0d73828cbe8c009d4fa1ffbae5378
SfcFromSx <woaifumu21>
2022-04-19 15:32:08 +0800

    更新

--- src/main/resources/static/ui/test_ui.js
+++ src/main/resources/static/ui/test_ui.js
@@ -3,6 +3,14 @@ const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
 const {MenuUnfoldOutlined, MenuFoldOutlined} = icons;
 
+const Page1 = () => {
+    <h1>page1</h1>
+}
+
+const Page2 = () => {
+    <h1>page2</h1>
+}
+
 
 class App extends React.Component {
     constructor() {
