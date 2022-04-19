0b1e70eab673ccc9da50ef1bb8e3d279190fe88b
SfcFromSx <woaifumu21>
2022-04-19 15:02:08 +0800

    更新

--- src/main/resources/static/ui/test_ui.js
+++ src/main/resources/static/ui/test_ui.js
@@ -2,59 +2,88 @@ const { Layout, Menu, Breadcrumb, Row, Col } = antd;
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
 
-const App = () => (
-    <Layout>
-        <Header style={{padding:0}} className="header">
-            <div style={{width: 200, height: '100%', textAlign:'center'}}>
-                <a id="logo">
-                    <img alt="logo" width={40} height={40} src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"/>
-                    <span style={{color:'blue', fontSize:24}}>管控平台</span>
-                </a>
-                {/*<h1 style={{color:'white', fontSize:24, textAlign:'center'}}>管控平台</h1>*/}
-            </div>
-        </Header>
-        <Layout>
-            <Sider width={200} className="site-layout-background">
-                <Menu
-                    theme="dark"
-                    mode="inline"
-                    defaultSelectedKeys={['1']}
-                    defaultOpenKeys={['sub1']}
-                    style={{ height: '100%', borderRight: 0 }}
-                >
-                    <SubMenu key="sub1" title="大屏设计">
-                        <Menu.Item key="1">模板设计</Menu.Item>
-                        <Menu.Item key="2">可视化编辑</Menu.Item>
-                    </SubMenu>
-                    <SubMenu key="sub2" title="业务设计">
-                        <Menu.Item key="5">DAG设计</Menu.Item>
-                        <Menu.Item key="6">模型配置</Menu.Item>
-                        <Menu.Item key="7">option7</Menu.Item>
-                        <Menu.Item key="8">option8</Menu.Item>
-                    </SubMenu>
-                    <SubMenu key="sub3" title="系统配置">
-                        <Menu.Item key="9">option9</Menu.Item>
-                        <Menu.Item key="10">option10</Menu.Item>
-                        <Menu.Item key="11">option11</Menu.Item>
-                        <Menu.Item key="12">option12</Menu.Item>
-                    </SubMenu>
-                </Menu>
-            </Sider>
-            <Layout style={{ padding: '0 24px 24px' }}>
-                <Content
-                    className="site-layout-background"
-                    style={{
-                        padding: 24,
-                        margin: 0,
-                        minHeight: 800,
-                    }}
-                >
-                    Content
-                </Content>
-            </Layout>
-        </Layout>
-    </Layout>
+const Page1 = () => (
+    <a>关闭</a>
 )
+
+const Page2 = () => (
+    <a>打开</a>
+)
+
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            collapsed: false,
+        }
+    }
+
+    toggle = () => {
+        console.log('toggle');
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    };
+
+    render() {
+        return (
+            <Layout>
+                <Header style={{padding:0}} className="header">
+                    <div style={{width: 200, height: '100%', textAlign:'center', float:'left'}}>
+                        <a id="logo">
+                            <img alt="logo" width={25} height={25} src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"/>
+                            <span style={{color:'white', fontSize:16}}> Vision Platform </span>
+                        </a>
+                        {/*<h1 style={{color:'white', fontSize:24, textAlign:'center'}}>管控平台</h1>*/}
+                    </div>
+                    <div style={{float:'left'}}>
+                        {React.createElement(this.state.collapsed ? Page1 : Page2, {
+                            className: 'trigger',
+                            onClick: this.toggle,
+                        })}
+                    </div>
+                </Header>
+                <Layout>
+                    <Sider className="site-layout-background" trigger={null} collapsible collapsed={this.state.collapsed}>
+                        <Menu
+                            theme="dark"
+                            mode="inline"
+                            defaultSelectedKeys={['1']}
+                            defaultOpenKeys={['sub1']}
+                            style={{ height: '100%', borderRight: 0 }}
+                        >
+                            <SubMenu key="sub1" title="大屏设计">
+                                <Menu.Item key="1">模板设计</Menu.Item>
+                                <Menu.Item key="2">页面编排</Menu.Item>
+                                <Menu.Item key="3">模块设计</Menu.Item>
+                                <Menu.Item key="4">图表设计</Menu.Item>
+                            </SubMenu>
+                            <SubMenu key="sub2" title="业务设计">
+                                <Menu.Item key="5">DAG设计</Menu.Item>
+                                <Menu.Item key="6">模型配置</Menu.Item>
+                                <Menu.Item key="7">option7</Menu.Item>
+                                <Menu.Item key="8">option8</Menu.Item>
+                            </SubMenu>
+                            <Menu.Item key="sub4">系统配置</Menu.Item>
+                        </Menu>
+                    </Sider>
+                    <Layout style={{ padding: '0 24px 24px' }}>
+                        <Content
+                            className="site-layout-background"
+                            style={{
+                                padding: 24,
+                                margin: 0,
+                                minHeight: 800,
+                            }}
+                        >
+                        </Content>
+                    </Layout>
+                </Layout>
+            </Layout>
+        );
+    }
+};
 ReactDOM.render(
     <App />,
     document.querySelector("#root")
